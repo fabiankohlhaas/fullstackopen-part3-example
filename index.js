fc6afa18a2bf48d44e662341d087855cef9a1a77
Fabian Kohlhaas <fabian.kohlhaas@plusserver.com>
2023-06-21 18:00:17 +0200

    Connect backend to mongo db database

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const Note = require('./models/note')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -15,46 +17,35 @@ app.use(requestLogger)
 app.use(cors())
 app.use(express.static('build'))
 
-
-let notes = [
-    {
-      id: 1,
-      content: "HTML is easy",
-      important: true
-    },
-    {
-      id: 2,
-      content: "Browser can execute only JavaScript",
-      important: false
-    },
-    {
-      id: 3,
-      content: "GET and POST are the most important methods of HTTP protocol",
-      important: true
-    }
-  ]
-
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  response.json(notes)
+  Note.find({}).then(notes => {
+    response.json(notes)
+  })
 })
 
+// app.get('/api/notes/:id', (request, response) => {
+//     const id = Number(request.params.id)
+//     console.log(id)
+//     const note = notes.find(note => {
+//         console.log(note.id, typeof note.id, id, typeof id, note.id === id)
+//         return note.id === id   
+//     })
+//     console.log(note)
+//     if (note) {
+//         response.json(note)
+//       } else {s
+//         response.status(404).end()
+//       }
+// })
+
 app.get('/api/notes/:id', (request, response) => {
-    const id = Number(request.params.id)
-    console.log(id)
-    const note = notes.find(note => {
-        console.log(note.id, typeof note.id, id, typeof id, note.id === id)
-        return note.id === id   
-    })
-    console.log(note)
-    if (note) {
-        response.json(note)
-      } else {s
-        response.status(404).end()
-      }
+  Note.findById(request.params.id).then(note => {
+    response.json(note)
+  })
 })
 
 app.delete('/api/notes/:id', (request, response) => {
@@ -64,31 +55,30 @@ app.delete('/api/notes/:id', (request, response) => {
     response.status(204).end()
 })
 
-const generateId = () => {
-    const maxId = notes.length > 0
-      ? Math.max(...notes.map(n => n.id))
-      : 0
-    return maxId + 1
-}
+// TODO continue with "Verifying frontend and backend integration"
+
+// const generateId = () => {
+//     const maxId = notes.length > 0
+//       ? Math.max(...notes.map(n => n.id))
+//       : 0
+//     return maxId + 1
+// }
   
 app.post('/api/notes', (request, response) => {
-const body = request.body
+  const body = request.body
 
-    if (!body.content) {
-        return response.status(400).json({ 
-        error: 'content missing' 
-        })
-    }
+  if (body.content === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
 
-    const note = {
-        content: body.content,
-        important: body.important || false,
-        id: generateId(),
-    }
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+  })
 
-    notes = notes.concat(note)
-
-    response.json(note)
+  note.save().then(savedNote => {
+    response.json(savedNote)
+  })
 })
 
 const unknownEndpoint = (request, response) => {
@@ -97,8 +87,9 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
 
+// mongodb+srv://fullstack:<password>@fso-c1.6aqry4p.mongodb.net/?retryWrites=true&w=majority
---
+++ mongo.js
@@ -0,0 +1,38 @@
+const mongoose = require('mongoose')
+
+if (process.argv.lengthcons3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+  `mongodb+srv://fullstack:${password}@fso-c1.6aqry4p.mongodb.net/noteApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery',false)
+mongoose.connect(url)
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+})
+
+const Note = mongoose.model('Note', noteSchema)
+
+// const note = new Note({
+//   content: 'CSS is Evil',
+//   important: true,
+// })
+
+// note.save().then(result => {
+//   console.log('note saved!')
+//   mongoose.connection.close()
+// })
+
+Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
